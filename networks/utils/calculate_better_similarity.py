c34ffa0857887774f20d5e86090e874109e8041f
Magyari-Saska Attila <atti2002@gmail.com>
2026-01-05 12:52:03 +0200

    Created monthly networks

--- networks/utils/calculate_better_similarity.py
+++ networks/utils/calculate_better_similarity.py
@@ -5,7 +5,7 @@ def calculate_similarity(
     month1_data,
     month2_data,
     stats,  # <-- NEW: global means & stds
-    weights={'deriv': 0.2, 'tg': 0.4, 'tn': 0.2, 'tx': 0.2}
+    weights={'deriv': 0.5, 'tg': 0.3, 'tn': 0.1, 'tx': 0.1}
 ):
     """
     stats = {
